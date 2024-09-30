6ec36b2f8f390f13ffc91650d188c3eb5bed2fdc
Greg Gladish <xray.dug@gmail.com>
2024-09-30 16:39:06 -0500

    Initial Commit

---
+++ BTTesting/BTTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTTestingApp.swift
+//  BTTesting
+//
+//  Created by Gregory Gladish on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BTTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BTTestingTests/BTTestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  BTTestingTests.swift
+//  BTTestingTests
+//
+//  Created by Gregory Gladish on 9/30/24.
+//
+
+import Testing
+@testable import BTTesting
+
+struct BTTestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
